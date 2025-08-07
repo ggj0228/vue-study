e85d2241bd9f5ce8fd0d0bf51ffb87007bbff0bb
wooyoungking <ggj0228@naver.com>
2025-08-07 12:19:43 +0900

    first vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <!--화면의 요소는 temaplate 안에 코딩-->>
+
 </template>
 
+<!-- js코드는 script안에 코딩-->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import HelloWorld from './components/HelloWorld.vue';
+ // vue는 export을 통해 해당 요소를 다른 화면에서도 import할 수 있도록 설정
+ // 즉 아래 문법은 App.vue하는 화면을 다른 파일에서도 import할 수 있도록 컴포넌트화시킨 것.
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'HelloWorld',
+  props: {
+    msg: String
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<!-- 디자인 요소는 스타일 태그 안에 코딩 -->
+<style scoped>
+
 </style>
